98b576ec5b81340fa608421258315c20fbdd0c20
aesw2010 <aesw2010@gmail.com>
2022-06-20 22:05:51 -0400

    primera a cuarta clase

---
+++ valores.js
@@ -0,0 +1,12 @@
+40
+
+"Alejandro Silva"
+
+true
+false
+
+null
+undefined
+
+[1,2,3]
+{nombre: "Diego"}
---
+++ variables.js
@@ -0,0 +1,17 @@
+var nombre = "Alejandro";
+
+// declarar
+var edad;
+//inicializar
+edad = 30;
+
+edad
+
+var elementos = ["computadora","celular"];
+
+var persona = {
+    Nombre:"Diego",
+    edad: 30
+}
+
+persona;
